d4cb2ecb74b586f8173bbf39272503da4063c3c4
Avinash <avinashmajhi345@gmail.com>
2026-07-21 08:57:26 +0530

    changes

---
+++ 04 javascript/lec 3 - user interaction, operator/operator.js
@@ -0,0 +1,31 @@
+console.log(2 ** 3)
+console.log(4 ** 3)
+
+//let x = 10
+//x **= 2 //x = x **2 
+//console.log(x)
+
+console.log(5 == "5")
+console.log(5 == "4")
+console.log(5 == "")
+console.log(1 == true)
+console.log(0 == false)
+
+console.log(5 == "5")
+console.log(1 == true)
+console.log(0 == false)
+
+console.log(5 != "5")
+console.log(3 != true)
+
+// 2 * 2^3
+console.log(2 << 3)
+console.log(8 << 5) // 8 * 2^5 //256
+
+console.log(64 >> 5) //64 / 2^5 //2
+console.log(356 >> 3) //256 / 2^3 //32
+
+// >>> (unsigned right shift)
+
+let x = 55, y = 56
+console.log(x > y ? x : y)
---
+++ 04 javascript/lec 3 - user interaction, operator/user interaction.js
@@ -0,0 +1,19 @@
+//alert("Hello Everyone!")
+//window.alert("Good morning")
+
+//let name = prompt("Enter your name:-")
+//console.log(name)
+//
+//if(name){
+    //console.log("your name is", name)
+//}else{
+   // console.log("Re enter user name")
+   //console.log("name is", prompt("Re enter user name"))
+//}
+
+//let choice = confirm("Are you enjoying classes!")
+//console.log(choice)
+
+console.log("Hii")      //normal
+console.warn("Hello")    //yellow
+console.error("Bye")      //red
