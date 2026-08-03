2c31f42f28df095550c02da32c841c8afd32f7e3
Avinash <avinashmajhi345@gmail.com>
2026-08-03 09:32:14 +0530

    lec 6

---
+++ 04 javascript/lec 6 - Higher order fucntions, Clousre/script.js
@@ -0,0 +1,78 @@
+// // function fun(f1) {
+// //   console.log("value of f1", f1)
+// // }
+
+// // // fun(10)
+// // // fun(true)
+// // // fun("JT")
+
+// // function hello() {
+// //   console.log("Hello Everyone!!!")
+// //   return 100
+// // }
+
+// // // fun(hello())
+// // fun(hello)
+
+
+// // ======================
+// function f3() {
+//   console.log("This is f3 function")
+// }
+
+// function f1() {
+//   console.log('This is f1 function')
+
+//   // return 101
+//   // return function f2() {
+//   //   console.log('This is f2 function')
+//   // }
+//   return f3
+// }
+
+// let x = f1()
+// console.log("//////////", x)
+// x()
+
+
+// ============== setTimeout, setInterval =========
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// console.log(1)
+// setTimeout(f1, 3000)
+// console.log(3)
+
+
+//function f1() {
+ // console.log("Hiiiiiii")
+//}
+
+//const intervalId = setInterval(f1, 2000)
+//console.log("/////////////", intervalId)
+  //console.log("Clearing interval")
+  //clearInterval(intervalId)
+//}, 10000)
+
+//function f2() {
+//  console.log("I am Avinash..")
+//}
+
+//const IntervalId = setInterval(f2, 2000)
+////console.log("/////////////", IntervalId)
+//setTimeout(function () {
+  //console.log("setting Timeout")
+ // clearTimeout(IntervalId)
+//}, 10000)
+
+// setTimeout()
+
+// ======================
+//console.log(1)
+//const id = setInterval
+//console.log(1)
+//setTimeout(function () {
+ //       console.log(":::::::")
+//}, 1000)
+//console.log(2)
