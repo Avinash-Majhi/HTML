6b9de56618517ffa310f77a217e2754798743dcc
Avinash <avinashmajhi345@gmail.com>
2026-08-03 09:32:47 +0530

    lec 7

---
+++ 04 javascript/lec 7 - function expression, arrow functions and  IIFE/script.js
@@ -0,0 +1,39 @@
+//const hello = function greet(){
+    //console.log('Hello Eveyone!!');
+//}
+
+//greet()
+//hello()
+
+//const concatString = function concatStrings(a, b){
+    return a + b;
+//}
+//console.log("///////" + concatString());
+//console.log(concatString("Java", "Script"));
+
+// ============= Anonymous function =============
+//Function = (a){
+ //console.log("........", a);
+
+//}          
+
+
+// ============= Arrow function =============
+
+//onst add = (num1, num2) => (num1 + num2){
+//console.log("Adding num1, num2");
+//return num1 + num2;
+//}
+
+//const result = add(10, 20);
+//const result1 = add(10);
+
+//console.log( "result is." , result);
+
+//============ IIFE =============
+(function(){
+    console.log("Heloooooooooooooooo", name);
+})("Avi");
+
+
+console.log("Java Technocrat");
